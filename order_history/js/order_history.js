746fb86c4a375bf3bd0b411f44683c624bbcf6a3
Mathangi <mathangi1993@gmail.com>
2018-04-22 20:30:56 -0500

    order history fix

--- order_history/js/order_history.js
+++ order_history/js/order_history.js
@@ -4,13 +4,6 @@ var minPassLen = 8,
 var passwordMsg = "Password must greater than " + minPassLen + " characters";
 var usernameMsg = "Please enter a valid email address";
 var matchpassword = "The Passwords do not match";
-jQuery.validator.setDefaults({
-    //Avoids form submit. Comment when in production.
-    success: "valid",
-    submitHandler: function() {
-        alert("Success! The form was pretend-submitted!");
-    }
-});
 (function(yourcode) {
     // The global jQuery object is passed as a parameter
     yourcode(window.jQuery, window, document);
